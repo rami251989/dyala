610b7a796b25602298d75bf8c967e28f05f62818
Rami <rami@logatta.com>
2025-10-15 21:45:22 +0300

    //

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import qrcode
 import tempfile
+import arabic_reshaper
+from bidi.algorithm import get_display
+
 
 
 
